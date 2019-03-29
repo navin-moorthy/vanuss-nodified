2e565be1bf0fbe30f0964b0bb81e5dd0f58f36c9
NavinNavi19 <navin007.a@gmail.com>
2019-03-29 16:54:07 +0000

    prettierinstall

--- app.js
+++ app.js
@@ -3,45 +3,45 @@
 /**
  * Express Module Dependencies
  */
- 
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
 
 const app = express();
 
 /**
  * view engine setup
  */
- 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // app.set("env", "production");
 
 /**
  * use module dependencies
  */
- 
-app.use(logger('dev'));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Routes
  */
- 
-app.use('/', indexRouter);
+
+app.use("/", indexRouter);
 
 /**
  * catch 404 and forward to error handler
  */
- 
+
 app.use((req, res, next) => {
   next(createError(404));
 });
@@ -49,15 +49,14 @@ app.use((req, res, next) => {
 /**
  * error handler
  */
- 
+
 app.use((err, req, res, next) => {
-  
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app
+module.exports = app;
